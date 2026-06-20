955519d0be9634483946f50e1df8fb482dc2679a
gabriel c <gabrielc.s.84133515@gmail.com>
2026-06-19 22:40:47 -0300

    ordem de gatilho entre botoes: ❌

---
+++ Assets/Scripts/Geral/ClickSeguro.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Animator))]
+public class ClickSeguro : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    private static bool entradaBloqueada = false;
+
+    private bool ponteiroDesceuAqui = false;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {       
+        entradaBloqueada = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (entradaBloqueada)
+        {
+            return; 
+        }
+
+        entradaBloqueada = true;
+        ponteiroDesceuAqui = true;
+
+        animator.SetTrigger("Selected");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        
+        if (!ponteiroDesceuAqui)
+        {
+            eventData.eligibleForClick = false;
+        }
+
+        ponteiroDesceuAqui = false;
+    }
+}
--- Assets/Scripts/Geral/MainMenu.cs
+++ Assets/Scripts/Geral/MainMenu.cs
@@ -18,7 +18,11 @@ public class MainMenu : MonoBehaviour
 
     public void Quit()
     {
-        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
     }
 
     public void AbrirOpcoes()
