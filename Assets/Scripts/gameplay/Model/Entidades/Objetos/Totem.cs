9e1ec48fb230b741d6f1389d7d6cd533b1f1cd7b
gabriel c <gabrielc.s.84133515@gmail.com>
2026-09-07 20:36:02 -0300

    totem posicionado e interagivel: ✔

---
+++ Assets/Scripts/gameplay/Model/Entidades/Objetos/Totem.cs
@@ -0,0 +1,30 @@
+﻿using Assets.Scripts.Gameplay.View.AnimacaoObjetos;
+using Assets.Scripts.Model.Entidades.Objetos.UtilitariosObjetos;
+using Assets.Scripts.View.AnimacaoObjetos;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Model.Entidades.Objetos
+{
+    public class Totem : MonoBehaviour, IInteracoes
+    {
+        private AnimTotem animacao;        
+        private StatusQueryOptions statusTotem { get; set; } // Enums, adicionar posteriormente
+
+        private void Awake()
+        {
+            animacao = GetComponent<AnimTotem>();
+
+            if (animacao == null) animacao = gameObject.AddComponent<AnimTotem>();
+        }
+
+        public void SofrerInteracao(Jogador jogador)
+        {
+            animacao.animacaoAtivando();
+        }
+        //travar camera()
+
+        //sinal de spawnar inimigos()
+    }
+}
---
+++ Assets/Scripts/gameplay/View/AnimacaoObjetos/AnimTotem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Gameplay.View.AnimacaoObjetos
+{
+	public class AnimTotem : MonoBehaviour
+	{
+
+        Animator animator;
+        void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+        public void animacaoAtivando()
+        {
+            animator.SetTrigger("ativar");
+        }
+    }
+}
