9ec1ef9bb3628e4d159c192f55390fd9818ed521
gabriel c <gabrielc.s.84133515@gmail.com>
2026-09-08 12:26:56 -0300

    Inicio do funcionamento dos huds de vida

---
+++ Assets/Scripts/Geral/VidaUIController.cs
@@ -0,0 +1,53 @@
+using Assets.Scripts.Model.Entidades.Peoes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VidaUIController : MonoBehaviour
+{
+
+    public int vida;
+    public int vidaMax;
+
+    public Image[] coracao;
+    public Sprite cheio;
+    public Sprite vazio;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LogicaCoracao();
+    }
+
+    void LogicaCoracao()
+    {
+        if(vida > vidaMax)
+        {
+            vida = vidaMax;
+        }
+
+        for (int i = 0; i < coracao.Length; i++)
+        {
+            if(i < vida)
+            {
+                coracao[i].sprite = cheio;
+            }
+            else
+            {
+                coracao[i].sprite = vazio;
+            }
+            if(i < vidaMax)
+            {
+                coracao[i].enabled = true;
+            }
+            else
+            {
+                coracao[i].enabled = true;
+            }
+        }
+    }
+}
